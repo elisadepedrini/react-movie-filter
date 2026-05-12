647fb74a68ea31871f9c76214ce3db3ef8899e52
elisadepedrini <elisa.depedrini@gmail.com>
2026-05-12 14:06:24 +0200

    Create html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-
+import { list_film } from "./data/app_data";
 
 function App() {
-  
 
   return (
     <>
-      
+      <header>
+        <h1>ELI'S FILM</h1>
+      </header>
+
+      <main>
+        {/* INPUT */}
+        <select className="form-select w-25 mx-auto mb-5" id="floatingSelectGrid">
+          <option>Quale genere di film vuoi guardare oggi?</option>
+          <option >Fantascienza</option>
+          <option >Thriller</option>
+          <option >Romantico</option>
+          <option >Azione</option>
+        </select>
+        
+
+        {/* CARD FILM */}
+        <div className="d-flex justify-content-around">
+          {list_film.map((film, index) => (
+            <div className="card text-start" key={index}>
+              <div className="card-body">
+                <h4 className="card-title">{film.title}</h4>
+                <p className="card-text">{film.genre}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/app_data.js
@@ -0,0 +1,10 @@
+const list_film = [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ]
+
+ export {list_film}
